9e9431e36c2740ffa3e103a465d2e7ae77e5ce18
Oualid <d14122972@mydit.ie>
2014-11-13 15:59:25 +0000

    add some code

--- list-was-queue.py
+++ list-was-queue.py
@@ -8,8 +8,19 @@ import boto.sqs.queue
 from boto.sqs.message import Message
 from boto.sqs.connection import SQSConnection
 from boto.exception import SQSError
+
 				#				A to B							t to m	
 conn = boto.sqs.connect_to_region("us-east-1", aws_access_key_id='BKIAIR7EH3TNSTDUCWKA', aws_secret_access_key='m2FZT5mrLYy8gX7kS1q0p4ObQYXTwGnaiUm+rxHZ')
+newConn = conn.create_queue("D14122972")
+new.set_message_class(Message)
+m = Message()
+m.set_body('It works yuuuur')
+newConn.write(m)
+
+textMessage = newConn.get_messages()
+print textMessage
+
+
 
 rs = conn.get_all_queues()
 for q in rs:
